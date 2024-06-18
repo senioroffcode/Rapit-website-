82682516680909fa26022c31d33bd86a8abd67ee
SenyorOFF <130892770+senioroffcode@users.noreply.github.com>
2024-06-18 16:20:28 +0500

    Add files via upload

---
+++ web/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Information)
+admin.site.register(Banner)
+admin.site.register(About)
+admin.site.register(Service)
+admin.site.register(Whywe)
+admin.site.register(Number)
+admin.site.register(Feature)
+admin.site.register(Category)
+admin.site.register(Portfolio)
+admin.site.register(Testimonials)
+admin.site.register(Team)
+admin.site.register(Xamkor)
+admin.site.register(Faq)
+admin.site.register(Contact)
